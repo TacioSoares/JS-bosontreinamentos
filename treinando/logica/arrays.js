3f239aabb7a158b854d08480becc95210d71aba2
Tácio Soares <taciofsoares@gmail.com>
2022-10-31 13:26:21 -0300

    22

--- treinando/logica/arrays.js
+++ treinando/logica/arrays.js
@@ -1,11 +1,14 @@
 var mes = ["Janeiro","Feveiro","Março","Abril","Maio","Junho","Julho","Agosto","Setembro","Outubro","Novembro"]
-var data = new Date
 var res = window.document.querySelector("#res")
 var botao = window.document.querySelector("#submeter")
-res.innerHTML = `Hoje é dia ${data.getDate()} de ${mes[data.getMonth()]} de ${data.getFullYear()}`
 botao.addEventListener('click',alterar)
-
+data = new Date()
+res.innerHTML = `Hoje é dia ${data.getDate()} de ${mes[data.getMonth()]} de ${data.getFullYear()}`
 function alterar() {
-    data = new Date(document.querySelector("#ano").value, document.querySelector("#meses").value, document.querySelector("#data").value)
-    return data
+    data.setDate(document.querySelector("#data").value)
+    data.setMonth(document.querySelector("#meses").value)
+    data.setFullYear(document.querySelector("#ano").value)
+    res.innerHTML = `Hoje é dia ${data.getDate()} de ${mes[data.getMonth()]} de ${data.getFullYear()}`
 }
+ 
+
